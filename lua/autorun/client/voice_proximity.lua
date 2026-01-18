0ea94d964eb56b8ff15ac3662c2d6464dc2bac2c
Mikey-Mikey <mikeygmoore07@gmail.com>
2026-01-18 01:37:48 -0500

    init commit

--- lua/autorun/client/voice_proximity.lua
+++ lua/autorun/client/voice_proximity.lua
@@ -1,12 +1,10 @@
 local voiceFalloff = 200
 
 hook.Add( "Think", "Voice_Proximity_Think", function()
-
         for _, ply in ipairs( player.GetHumans() ) do
             if ply == LocalPlayer() then continue end
             ply:SetVoiceVolumeScale( 1 / ( 1 + ( ply:GetShootPos() + ply:GetVelocity() * FrameTime() * 90 ):Distance( LocalPlayer():GetShootPos() + LocalPlayer():GetVelocity() * FrameTime() * 90 )^2 / voiceFalloff^2 ) )
         end
-	
 end )
 
 hook.Add( "PlayerStartVoice", "Voice_Proximity_PlayerStartVoice", function( ply )
